de96d39e6b54a011a0b1fd33057990ff08326ae8
ObservWard <71263841+ObservWard@users.noreply.github.com>
2020-09-14 16:08:35 +0400

    Update Main.java

--- Массив2/src/com/company/Main.java
+++ Массив2/src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String args[])
     {
         int[] array = {1, 5, 4, 3, 7};
-        System.out.println(Arrays.toString(array));
         Arrays.sort(array, 0, 4);
+        System.out.println(Arrays.toString(array));
     }
 }
